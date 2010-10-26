fc0db2146ef3402be31e349539b98a87726310f5
Wu Haibo <wuhaibo@ceopen.cn>
2010-10-26 11:29:50 +0800

    replace url of homepage

--- url_replace.js
+++ url_replace.js
@@ -1,10 +1,14 @@
 function url_replace() {
 	var links = document.getElementsByTagName("a");
+	var replaced = false;
 	for(var i = 0; i<links.length; i++) {
 		if (links[i].href.indexOf('http://forum.xitek.com/showthread.php') > -1) {
 			links[i].href = links[i].href.replace ("showthread.php", "sorthread.php"); 
+			replaced = true;
 		}
 	}
+	if (replaced)
+		chrome.extension.sendRequest({}, function(response) {});
 }
 
 url_replace();
