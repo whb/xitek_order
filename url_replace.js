01c3eaaa27e75256e753868b67f5e46e4c89cadd
Wu Haibo <wuhaibo@ceopen.cn>
2010-10-27 09:21:44 +0800

    Don't replace desc_order url in post detail page.

--- url_replace.js
+++ url_replace.js
@@ -2,7 +2,8 @@ function url_replace() {
 	var links = document.getElementsByTagName("a");
 	var replaced = false;
 	for(var i = 0; i<links.length; i++) {
-		if (links[i].href.indexOf('http://forum.xitek.com/showthread.php') > -1) {
+		if (links[i].href.indexOf('http://forum.xitek.com/showthread.php') > -1 &&
+			  links[i].innerHTML.indexOf('倒序显示') == -1 ) {
 			links[i].href = links[i].href.replace ("showthread.php", "sorthread.php"); 
 			replaced = true;
 		}
